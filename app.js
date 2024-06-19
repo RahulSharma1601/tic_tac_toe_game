c667d0eec2d364ba5dec648dfa7fd1197e307aef
RahulSharma1601 <157831356+RahulSharma1601@users.noreply.github.com>
2024-06-19 11:57:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+
+let turn0 = true;
+
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("buttum was click");
+        if (turn0) {
+            box.innerText = "0"
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        CheckWinner();
+
+    });
+});
+const CheckWinner = () => {
+    for (let pattern of winpatterns) {
+        
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                console.log("winner",pos1val);
+            }
+        }
+    }
+
+}
